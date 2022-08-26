f5cac30f56eb6893ee8ec8c5a52da393e1424463
YuiOgawa <yui20010218@gmail.com>
2022-08-26 15:29:57 +0900

    :sparkles: 教科書1-2終了.

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    // ドラムを表示する箱を作る
+    @IBOutlet var drumButton: UIButton!
+    
     // ドラムのサウンドファイルを読み込んで、プレイヤーをつくる
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
     
@@ -17,12 +20,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     // ドラムがタップされたときに呼ばれる
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        // ドラムが鳴っている画像に切り替える
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         // ドラムの音を巻き戻す
         drumSoundPlayer.currentTime = 0
         // ドラムの音を再生する
         drumSoundPlayer.play()
     }
-
+    //ドラムボタンの上でタッチが終わる時に呼ばれる
+    @IBAction func touchUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
 }
 
